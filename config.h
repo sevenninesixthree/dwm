31ff495c267f8107d2c27cb8e3eeacb0ea57fb1e
sevenninesixthree <1875488550@qq.com>
2025-01-01 14:23:03 +0800

    change modkey from alt to super

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -17,7 +17,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#2c5b0d";
-static const char col_hint[]        = "#6cab0d";
+static const char col_hint[]        = "#208b6d";
 static const unsigned int baralpha = 0x80;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -30,6 +30,7 @@ static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
   [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeBar]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -59,7 +60,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define MODKEY2 Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -85,14 +87,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|MODKEY2,               XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|MODKEY2,               XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|MODKEY2|ShiftMask,     XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|MODKEY2|ShiftMask,     XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|MODKEY2|ControlMask,   XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|MODKEY2|ControlMask,   XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|MODKEY2,               XK_0,      togglegaps,     {0} },
+	{ MODKEY|MODKEY2|ShiftMask,     XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
